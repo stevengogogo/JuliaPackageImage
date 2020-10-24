aac10ee169f755a06818988919d91ebb00c30690
Steven Chiu <stevengogogo4321@gmail.com>
2020-10-24 17:02:10 +0800

    create package template

---
+++ conservative_package_compiler_testing.jl
@@ -0,0 +1,48 @@
+using PackageCompiler
+using Pkg
+
+function installed()
+    deps = Pkg.dependencies()
+    installs = Dict{String, VersionNumber}()
+    for (uuid, dep) in deps
+        dep.is_direct_dep || continue
+        dep.version === nothing && continue
+        installs[dep.name] = dep.version
+    end
+    return installs
+end
+
+function adduninstalled(pkgs)
+    for i in pkgs
+        i_str = String(i)
+        if i_str ∉ keys(installed())
+            Pkg.add(String(i))
+        end
+    end
+end
+
+
+# Add your packages here
+conservative = [
+    :ForwardDiff
+    :Plots
+    :StaticArrays
+    :BenchmarkTools
+    :Parameters
+    :NLsolve
+    :FiniteDiff
+    :LabelledArrays
+    :DifferentialEquations
+]
+
+# Add packages if not exists
+adduninstalled(conservative)
+
+
+diffeqbase = [
+    :DiffEqBase
+]
+
+
+
+PackageCompiler.create_sysimage(vcat(conservative,diffeqbase);replace_default=true)
